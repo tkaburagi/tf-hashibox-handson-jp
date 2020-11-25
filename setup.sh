5478805a7fc2badda7d21b980734507b2154d7c7
tkaburagi <t.kaburagi@me.com>
2020-11-25 17:22:52 +0900

    nomad ws

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ sudo apt install -y php
 ### Installing Golang
 wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.15.5.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/ubuntu/.bashrc
 
 ### HashiStack
 wget "${vault_url}" -O /home/ubuntu/vault.zip
